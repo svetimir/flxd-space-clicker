8849884890474f826dd846ab5a4c820883bc3744
svetimir <ifi@yandex.ru>
2018-09-12 10:58:44 +0300

    0.0.1

---
+++ main.py
@@ -0,0 +1,18 @@
+# AUTHOR: fluxoid, ifi@yandex.ru
+# VERSION: 0.0.1
+# STARTED: 12.09.2018
+# LATEST FILE REVISION: 12.09.2018
+# PURPOSE: Project "space clicker" main file
+# DESCRIPTION: Space clicker project main file
+
+class Star(object):
+    pass
+
+class ClickHandler(object):
+    pass
+
+class Stats(object):
+    pass
+
+def main():
+    pass
