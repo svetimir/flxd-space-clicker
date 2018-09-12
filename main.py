ca0ae96675ec7cbdbd963571c4953cbf8d29bd2d
svetimir <ifi@yandex.ru>
2018-09-12 11:29:48 +0300

    0.0.2

--- main.py
+++ main.py
@@ -5,14 +5,50 @@
 # PURPOSE: Project "space clicker" main file
 # DESCRIPTION: Space clicker project main file
 
-class Star(object):
-    pass
+# by clicking the star
+# you get star beams :)
 
-class ClickHandler(object):
-    pass
+import pygame
+from pygame.locals import *
 
-class Stats(object):
-    pass
+class Game(object):
+    def __init__(self):
+        self._running=True
+        self._display_surf=None
+        self.size=self.weight,self.height=640,480
+
+    def on_init(self):
+        pygame.init()
+        self._display_surf=pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running=True
+
+    def on_event(self,event):
+        if event.type==pygame.QUIT:
+            self._running=False
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        if self.on_init()==False:
+            self._running=False
+        while(self._running):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
 
 def main():
-    pass
+    g=Game()
+    g.on_execute()
+
+if __name__=="__main__":
+    main()
